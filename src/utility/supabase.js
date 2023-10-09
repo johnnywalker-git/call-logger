90f18f7c0fbb23dc69d728559c5f5195ad211d0e
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-10-09 15:37:13 +0100

    Remove filename from the repository

--- src/utility/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Replace these values with your actual Supabase project URL and API key
-const supabase = createClient('https://pjsnxhvysbwjfcnritql.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqc254aHZ5c2J3amZjbnJpdHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0MjI1MjAsImV4cCI6MjAxMTk5ODUyMH0.6uyBinlI0oskKCyzM2S0FMuC33wUMaTVOQSsnwgEnEI')
-
-export { supabase };
